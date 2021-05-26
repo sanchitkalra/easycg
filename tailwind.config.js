8ac1519b914d6c38e0014e6ca2ef3c48ffdb2080
Sanchit Kalra <sanchit.kalra1@gmail.com>
2021-05-26 18:24:01 +0530

    Init: Install Tailwind, Add basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.jsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
